4235fdc1ddf13d361e9028eb97c9e5743d5a747a
AgustinHenin <agustin.g.henin@gmail.com>
2024-09-12 01:10:37 -0300

    holaa

---
+++ Negocio/marca.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Marca
+    {
+        public string nombre { get; set; }
+        public string rubro { get; set; }
+        public string origen { get; set; }
+
+        public override string ToString()
+        {
+            return nombre;
+        }
+    }
+}
