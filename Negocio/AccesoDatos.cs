9045e398d9f686a40640926719dd50541de3d4a4
AgustinHenin <168901916+AgustinHenin@users.noreply.github.com>
2024-09-12 01:03:55 -0300

    Add files via upload

---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TPWinForm_equipo_5B
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=DESKTOP-TI2DHGA\\SQLEXPRESS; database=CATALOGO_P3_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void setearconsulta(string consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarlectura()
+        {
+            comando.Connection = conexion;
+            conexion.Open();
+            lector = comando.ExecuteReader();
+        }
+
+        public void cerrarconexion()
+        {
+            if(lector != null)
+            {
+                lector.Close();
+            }
+            conexion.Close();
+        }
+    }
+}
